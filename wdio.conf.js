04ab1b3891e2a0b6efd219c16f9d35d8cfe13db8
AshishMotanamGurunadham <motanamashish@gmail.com>
2020-07-29 12:13:58 -0500

    Update: test-7

--- wdio.conf.js
+++ wdio.conf.js
@@ -146,7 +146,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: `http://${WDIO_EXTERNAL_HOST || 'localhost'}:${WDIO_EXTERNAL_PORT || 9991}`,
+    baseUrl: `http://${WDIO_EXTERNAL_HOST || ip.address()}:${WDIO_EXTERNAL_PORT || 9991}`,
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
