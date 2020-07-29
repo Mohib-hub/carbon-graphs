7977c2b8ca81529870a55bcbb10c44b6bd4aaad4
AshishMotanamGurunadham <motanamashish@gmail.com>
2020-07-29 15:53:46 -0500

    Update: test-16

--- build/wdio/wdio.conf.js
+++ build/wdio/wdio.conf.js
@@ -146,7 +146,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: `http://${WDIO_EXTERNAL_HOST || 'localhost'}:${WDIO_EXTERNAL_PORT || 9991}`,
+    baseUrl:  process.env.CI ? "https://engineering.cerner.com/carbon-graphs" : `http://${WDIO_EXTERNAL_HOST || 'localhost'}:${WDIO_EXTERNAL_PORT || 9991}`,
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
     //
